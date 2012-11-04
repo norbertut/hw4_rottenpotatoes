2b4a4b7f0fc4fb23c1a3b5f45afd45b8859582b9
norbertut <norbertrovira@gmail.com>
2012-11-04 22:16:47 +0100

    final

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -8,9 +8,6 @@ describe MoviesController do
 			@input_movie = movies(:star_wars_movie)
 		end
 
-#modificar, no tiene que llamar al modelo si no al controller, o me equivoco? quizás un método find by director debería existir
-
-
 		it 'should call the model method that finds movies with same director' do
 			Movie.should_receive(:find_same_director).with(@input_movie.id.to_s)
 			get :samedirector, id: @input_movie
@@ -27,6 +24,65 @@ describe MoviesController do
 			get :samedirector, id: @input_movie
 			assigns(:movies).should == @fake_results
 		end
+
+	end
+
+	describe 'play with a movie' do
+		fixtures :movies
+		before :each do
+			@input_movie = movies(:star_wars_movie)
+		end
+
+		it 'should show a movie' do
+			response.should be_successful
+			get :show, id: @input_movie
+		end
+
+		it 'should destroy a movie' do
+			response.should be_successful
+			delete :destroy, id: @input_movie
+		end
+
+		it 'should edit a movie' do
+			response.should be_successful
+			get :edit, id: @input_movie
+		end
+
+		it 'should update a movie' do
+			response.should be_successful
+			put :update, id: @input_movie
+		end
+
+		it 'should show new view' do
+			response.should be_successful
+			get :new
+		end
+
+		it 'should show all movies' do
+			response.should be_successful
+			get :index
+		end
+
+		it 'should create a movie' do
+			response.should be_successful
+			post :create
+		end
+
+		it 'should show all movies sorted by title' do
+			response.should be_successful
+			get :index, :sort => 'title'
+		end
+
+		it 'should show all movies sorted by release_date' do
+			response.should be_successful
+			get :index, :sort => 'release_date'
+		end
+
+		it 'should show movies filtered by rating' do
+			response.should be_successful
+			get :index, :ratings => ['G']
+		end
+
 
 	end
 end
