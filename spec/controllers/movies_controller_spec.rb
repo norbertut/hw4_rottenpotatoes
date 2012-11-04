9876f2ccd858ae8c48d4c8d9a23f67f9bcd00917
norbertut <norbertrovira@gmail.com>
2012-11-04 20:49:44 +0100

    code working, tests working but low coverage

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -5,25 +5,26 @@ describe MoviesController do
 		fixtures :movies
 		before :each do
 			@fake_results = [movies(:star_wars_movie),movies(:thx1138_movie)]
+			@input_movie = movies(:star_wars_movie)
 		end
 
 #modificar, no tiene que llamar al modelo si no al controller, o me equivoco? quizás un método find by director debería existir
 
 
 		it 'should call the model method that finds movies with same director' do
-			MoviesController.should_receive(:samedirector).with("1")
-			get :samedirector, {:id => "1"}
+			Movie.should_receive(:find_same_director).with(@input_movie.id.to_s)
+			get :samedirector, id: @input_movie
 		end
 
-		it 'should select the Same Director template for rendering' do
+		it 'should render the template successfully' do
 			Movie.stub(:find_same_director).and_return(@fake_results)
-			get :samedirector, {:id => "1"}
-			response.should redirect_to('same_director')
+			response.should be_successful
+			get :samedirector, id: @input_movie
 		end
 
 		it 'should make the same director results available to the template' do
 			Movie.stub(:find_same_director).with("1").and_return(@fake_results)
-			get :samedirector, {:id => "1"}
+			get :samedirector, id: @input_movie
 			assigns(:movies).should == @fake_results
 		end
 
